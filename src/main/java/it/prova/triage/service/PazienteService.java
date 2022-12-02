4080651e934e4e18c47a813de21e2a3d3fa06ff9
Balraj1108 <balrajsingh1108@gmail.com>
2022-12-02 10:05:40 +0100

    Implementazione PazienteServiceImpl

---
+++ src/main/java/it/prova/triage/service/PazienteService.java
@@ -0,0 +1,18 @@
+package it.prova.triage.service;
+
+import java.util.List;
+
+import it.prova.triage.model.Paziente;
+
+public interface PazienteService {
+
+	public List<Paziente> listAll();
+
+	public Paziente caricaSingoloElemento(Long id);
+
+	public void aggiorna(Paziente pazienteInstance);
+
+	public Paziente inserisciNuovo(Paziente pazienteInstance);
+
+	public void rimuovi(Long idToRemove);
+}
---
+++ src/main/java/it/prova/triage/service/PazienteServiceImpl.java
@@ -0,0 +1,45 @@
+package it.prova.triage.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.prova.triage.model.Paziente;
+import it.prova.triage.model.StatoPaziente;
+import it.prova.triage.repository.paziente.PazienteRepository;
+
+@Service
+public class PazienteServiceImpl implements PazienteService {
+
+	@Autowired
+	private PazienteRepository pazienteRepository;
+
+	@Override
+	public List<Paziente> listAll() {
+		return (List<Paziente>) pazienteRepository.findAll();
+	}
+
+	@Override
+	public Paziente caricaSingoloElemento(Long id) {
+		return pazienteRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public void aggiorna(Paziente pazienteInstance) {
+		pazienteRepository.save(pazienteInstance);
+	}
+
+	@Override
+	public Paziente inserisciNuovo(Paziente pazienteInstance) {
+		pazienteInstance.setDataRegistrazione(LocalDate.now());
+		pazienteInstance.setStato(StatoPaziente.INT_ATTESA_VISITA);
+		return pazienteRepository.save(pazienteInstance);
+	}
+
+	@Override
+	public void rimuovi(Long idToRemove) {
+		pazienteRepository.deleteById(idToRemove);
+	}
+}
